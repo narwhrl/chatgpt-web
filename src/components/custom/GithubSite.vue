2117547e7d615b8fd9ad3c488bee5b1bfa0007c4
Narwhrl <78220282+narwhrl@users.noreply.github.com>
2023-03-31 14:08:54 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Bulit by</span>
+    <a href="https://github.com/narwhrl" target="_blank" class="text-blue-500">
+      Narwhrl
+    </a>
+		<a href="https://github.com/Chanzhaoyu/chatgpt-web" target="_blank" class="text-blue-500">
+      Source
     </a>
   </div>
 </template>
